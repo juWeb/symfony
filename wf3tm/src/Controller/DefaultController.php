cbce6ae7aa2b78c1313afb8074e993a11f4c2f28
juWeb <webert.ju57@gmail.com>
2018-08-02 10:25:43 +0200

    second commit

--- wf3tm/src/Controller/DefaultController.php
+++ wf3tm/src/Controller/DefaultController.php
@@ -1,11 +1,19 @@
 <?php
 
 namespace App\Controller;
-use Symfony\Component\HttpFoundation\Response;
 
-class DefaultController {
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller {
     public function homepage() {
-        return new Response('<html><body>Hello world !!!</body></html>');
+        $loggedState = true;
+        $elementList = range(1, 10);
+
+        return $this->render('example.html.twig', [
+            'isLogged' => $loggedState,
+            'elements' => $elementList
+        ]);
     }
 }
 
